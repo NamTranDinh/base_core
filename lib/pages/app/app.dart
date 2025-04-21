314434c5b3b02c9010ffa21ac8cce922a37c0a0f
will <will.cmcglobal@gmai.com>
2025-04-21 17:20:01 +0700

    base: app
    This commit introduces lifecycle state change monitoring to the app.
    
    It makes the `App` widget stateful and adds `WidgetsBindingObserver` to listen to changes in the application's lifecycle. When the app transitions between different states (resumed, inactive, paused, detached, hidden), debug logs are printed to the console, allowing developers to track and respond to these changes effectively. Additionally, it removes the observer in the dispose method.

--- lib/pages/app/app.dart
+++ lib/pages/app/app.dart
@@ -5,12 +5,40 @@ import 'package:base_core/cores/app_theme.dart';
 import 'package:base_core/di.dart';
 import 'package:base_core/routes/app_router.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      if (kDebugMode) print('AppLifecycleState.resumed');
+    } else if (state == AppLifecycleState.inactive) {
+      if (kDebugMode) print('AppLifecycleState.inactive');
+    } else if (state == AppLifecycleState.paused) {
+      if (kDebugMode) print('AppLifecycleState.paused');
+    } else if (state == AppLifecycleState.detached) {
+      if (kDebugMode) print('AppLifecycleState.detached');
+    } else if (state == AppLifecycleState.hidden) {
+      if (kDebugMode) print('AppLifecycleState.hidden');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,5 +67,11 @@ class App extends StatelessWidget {
         },
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
   }
 }
